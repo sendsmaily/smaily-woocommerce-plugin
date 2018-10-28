c6aca2734745cc324bfc902e04ad929c65e9bff2
Kaarel Rooparg <kaarel.rooparg@gmail.com>
2018-10-28 21:35:37 +0200

    Fixed bug with cron - database returns string instead of int.

--- inc/Base/Cron.php
+++ inc/Base/Cron.php
@@ -32,7 +32,7 @@ class Cron {
 
 		$results = DataHandler::get_smaily_results();
 		// Check if contact sync is enabled.
-		if ( $results['result']['enable'] === 1 ) {
+		if ( (int) $results['result']['enable'] === 1 ) {
 
 				// List value 2  = unsubscribers list.
 			$data = array(
