a3f54b36f21e6a3c4f6f9798023aa44b725216cd
Kaarel <kaarel.rooparg@gmail.com>
2020-02-10 20:50:10 +0200

    Make initial refractor of AC functionality

--- inc/Base/Cron.php
+++ inc/Base/Cron.php
@@ -113,148 +113,177 @@ class Cron {
 	 * @return void
 	 */
 	public function smaily_abandoned_carts_email() {
-		// WordPress Database handler.
-		global $wpdb;
 		// Get Smaily settings.
 		$results = DataHandler::get_smaily_results();
-		// Check if contact sync is enabled.
-		if ( isset( $results['result']['enable_cart'] ) && (int) $results['result']['enable_cart'] === 1 ) {
+		if ( ! isset( $results['result']['enable_cart'] ) ) {
+			// Something wrong with settings. Default value 0.
+			return;
+		}
 
-			// Get all abandoned carts.
-			$abandoned_carts = $wpdb->get_results(
-				"
-				SELECT * FROM {$wpdb->prefix}smaily_abandoned_carts
-				WHERE cart_status='abandoned'
-				AND mail_sent IS NULL
-				",
-				'ARRAY_A'
-			);
-			foreach ( $abandoned_carts as $cart ) {
-				// Get cart details and cart data from cart.
-				$cart_data = unserialize( $cart['cart_content'] );
-				// Continue with sending data to Smaily if there are items in customer cart.
-				if ( empty( $cart_data ) ) {
-					continue;
-				}
+		if ( (int) $results['result']['enable_cart'] !== 1 ) {
+			// Not activated.
+			return;
+		}
 
-				// Customer fields available.
-				$customer_id   = $cart['customer_id'];
-				$customer_data = get_userdata( $customer_id );
-				$customer      = [
-					'first_name' => $customer_data->first_name ? $customer_data->first_name : '',
-					'last_name'  => $customer_data->last_name ? $customer_data->last_name : '',
-					'email'      => $customer_data->user_email ? $customer_data->user_email : '',
-				];
-				// Continue with data gathering only if there is an email value to send data to.
-				if ( empty( $customer['email'] ) ) {
-					continue;
-				}
+		$abandoned_carts = $this->get_abandoned_carts();
+		foreach ( $abandoned_carts as $cart ) {
+			// Get cart details and cart data from cart.
+			$cart_data = unserialize( $cart['cart_content'] );
+			// Continue with sending data to Smaily if there are items in customer cart.
+			if ( empty( $cart_data ) ) {
+				continue;
+			}
 
-				// Data to send to smail API.
-				$addresses = [
-					'first_name' => '',
-					'last_name'  => '',
-				];
-				// Gather customer data.
-				$customer_data = [];
-				$sync_values   = [ 'first_name', 'last_name', 'email' ];
-				foreach ( $sync_values as $sync_value ) {
-					// Check if user has enabled extra field in settings.
-					if ( in_array( $sync_value, $results['cart_options'], true ) || $sync_value === 'email' ) {
-						// Add extra field if it's available in customer data.
-						if ( isset( $customer[ $sync_value ] ) ) {
-							$addresses[ $sync_value ] = $customer[ $sync_value ];
-						}
+			// Customer fields available.
+			$customer_id   = $cart['customer_id'];
+			$customer_data = get_userdata( $customer_id );
+			$customer      = [
+				'first_name' => $customer_data->first_name ? $customer_data->first_name : '',
+				'last_name'  => $customer_data->last_name ? $customer_data->last_name : '',
+				'email'      => $customer_data->user_email ? $customer_data->user_email : '',
+			];
+			// Continue with data gathering only if there is an email value to send data to.
+			if ( empty( $customer['email'] ) ) {
+				continue;
+			}
+
+			// Data to send to smail API.
+			$addresses = [
+				'first_name' => '',
+				'last_name'  => '',
+			];
+			// Gather customer data.
+			$customer_data = [];
+			$sync_values   = [ 'first_name', 'last_name', 'email' ];
+			foreach ( $sync_values as $sync_value ) {
+				// Check if user has enabled extra field in settings.
+				if ( in_array( $sync_value, $results['cart_options'], true ) || $sync_value === 'email' ) {
+					// Add extra field if it's available in customer data.
+					if ( isset( $customer[ $sync_value ] ) ) {
+						$addresses[ $sync_value ] = $customer[ $sync_value ];
 					}
-				}
-
-				// Get cart page url.
-				$cart_page_id          = wc_get_page_id( 'cart' );
-				$cart_page_url         = $cart_page_id ? get_permalink( $cart_page_id ) : '';
-				$addresses['cart_url'] = $cart_page_url;
-				// Products data values available.
-				$cart_sync_values = [
-					'product_name',
-					'product_description',
-					'product_sku',
-					'product_quantity',
-					'product_description_short',
-					'product_subtotal',
-				];
-				// Add empty product data for addresses. Fields available would be filled out later with data.
-				// Recuired for legacy API so that all fields are always updated.
-				foreach ( $cart_sync_values as $key ) {
-					for ( $i = 1; $i < 11; $i++ ) {
-						$addresses[ $key . '_' . $i ] = '';
-					}
-				}
-				// Gather products data if user has selected at least one of additional product field to sync.
-				if ( ! empty( array_intersect( $cart_sync_values, $results['cart_options'] ) ) ) {
-					$products_data = [];
-					foreach ( $cart_data as $cart_item ) {
-						// Single product detais array.
-						$product = [];
-						$details = wc_get_product( $cart_item['product_id'] ); // Get product by ID.
-						// Get product details if selected from user settings.
-						if ( in_array( 'product_name', $results['cart_options'], true ) ) {
-							$product['name'] = htmlspecialchars( $details->get_name() );
-						}
-						if ( in_array( 'product_description', $results['cart_options'], true ) ) {
-							$product['description'] = htmlspecialchars( $details->get_description() );
-						}
-						if ( in_array( 'product_sku', $results['cart_options'], true ) ) {
-							$product['sku'] = htmlspecialchars( $details->get_sku() );
-						}
-						if ( in_array( 'product_description_short', $results['cart_options'], true ) ) {
-							$product['description_short'] = htmlspecialchars( $details->get_short_description() );
-						}
-						if ( in_array( 'product_quantity', $results['cart_options'], true ) ) {
-							$product['quantity'] = htmlspecialchars( $cart_item['quantity'] );
-						}
-						if ( in_array( 'product_subtotal', $results['cart_options'], true ) ) {
-							$product['subtotal'] = htmlspecialchars( $cart_item['line_subtotal'] );
-						}
-						$products_data[] = $product;
-					}
-
-					// Append products array to API api call. Up to 10 product details.
-					$i = 1;
-					foreach ( $products_data as $product ) {
-						if ( $i <= 10 ) {
-							foreach ( $product as $key => $value ) {
-								$addresses[ 'product_' . $key . '_' . $i ] = htmlspecialchars( $value );
-							}
-							$i++;
-						}
-					}
-				}
-				// Query for Smaily autoresponder.
-				$query = [
-					'autoresponder' => $results['result']['cart_autoresponder_id'], // autoresponder ID.
-					'addresses'     => [ $addresses ],
-				];
-
-				// Send data to Smaily.
-				$response = Api::ApiCall( 'autoresponder', '', [ 'body' => $query ], 'POST' );
-				// If data sent successfully update mail_sent status in database.
-				if ( array_key_exists( 'code', $response ) && $response['code'] === 101 ) {
-					$table = $wpdb->prefix . 'smaily_abandoned_carts';
-					$wpdb->update(
-						$table,
-						array(
-							'mail_sent'      => 1,
-							'mail_sent_time' => gmdate( 'Y-m-d\TH:i:s\Z' ),
-						),
-						array(
-							'customer_id' => $customer_id,
-						)
-					);
-				} else {
-					// Log to file if errors.
-					$this->log_to_file( SMAILY_PLUGIN_PATH . 'smaily-cart.txt', wp_json_encode( $response ) );
 				}
 			}
+
+			// Get cart page url.
+			$cart_page_id          = wc_get_page_id( 'cart' ); // TODO: check if page ostukorv or smth?
+			$cart_page_url         = $cart_page_id ? get_permalink( $cart_page_id ) : '';
+			$addresses['cart_url'] = $cart_page_url;
+			// Products data values available.
+			$cart_sync_values = [
+				'product_name',
+				'product_description',
+				'product_sku',
+				'product_quantity',
+				'product_description_short',  // TODO: is it necessary?
+				'product_subtotal',			 // TODO: is it necessary?
+			];
+			// Add empty product data for addresses. Fields available would be filled out later with data.
+			// Required for legacy API so that all fields are always updated.
+			foreach ( $cart_sync_values as $key ) {
+				for ( $i = 1; $i < 11; $i++ ) {
+					$addresses[ $key . '_' . $i ] = '';
+				}
+			}
+			// Gather products data if user has selected at least one of additional product field to sync.
+			if ( ! empty( array_intersect( $cart_sync_values, $results['cart_options'] ) ) ) {
+				$products_data = [];
+				foreach ( $cart_data as $cart_item ) {
+					// Single product detais array.
+					$product = [];
+					$details = wc_get_product( $cart_item['product_id'] ); // Get product by ID.
+					// Get product details if selected from user settings.
+					if ( in_array( 'product_name', $results['cart_options'], true ) ) {
+						$product['name'] = htmlspecialchars( $details->get_name() );
+					}
+					if ( in_array( 'product_description', $results['cart_options'], true ) ) {
+						$product['description'] = htmlspecialchars( $details->get_description() );
+					}
+					if ( in_array( 'product_sku', $results['cart_options'], true ) ) {
+						$product['sku'] = htmlspecialchars( $details->get_sku() );
+					}
+					if ( in_array( 'product_description_short', $results['cart_options'], true ) ) {
+						$product['description_short'] = htmlspecialchars( $details->get_short_description() );
+					}
+					if ( in_array( 'product_quantity', $results['cart_options'], true ) ) {
+						$product['quantity'] = htmlspecialchars( $cart_item['quantity'] );
+					}
+					if ( in_array( 'product_subtotal', $results['cart_options'], true ) ) {
+						$product['subtotal'] = htmlspecialchars( $cart_item['line_subtotal'] );
+					}
+					// TODO: check product_price and product_base_price fields.
+					$products_data[] = $product;
+				}
+
+				// Append products array to API api call. Up to 10 product details.
+				$i = 1;
+				foreach ( $products_data as $product ) {
+					if ( $i <= 10 ) {
+						foreach ( $product as $key => $value ) {
+							$addresses[ 'product_' . $key . '_' . $i ] = htmlspecialchars( $value );
+						}
+						$i++;
+					}
+				}
+			}
+			// Query for Smaily autoresponder.
+			$query = [
+				'autoresponder' => $results['result']['cart_autoresponder_id'], // autoresponder ID.
+				'addresses'     => [ $addresses ],
+			];
+
+			// Send data to Smaily.
+			$response = Api::ApiCall( 'autoresponder', '', [ 'body' => $query ], 'POST' );
+			// If data sent successfully update mail_sent status in database.
+			if ( array_key_exists( 'code', $response ) && $response['code'] === 101 ) {
+				$this->update_mail_sent_status( $customer_id );
+			} else {
+				// Log to file if errors.
+				$this->log_to_file( SMAILY_PLUGIN_PATH . 'smaily-cart.txt', wp_json_encode( $response ) );
+			}
 		}
+	}
+
+	/**
+	 * Update mail_sent and mail_sent_time status in smaily_abandoned_carts table.
+	 *
+	 * @param int $customer_id Customer ID
+	 * @return void
+	 */
+	public function update_mail_sent_status( $customer_id ) {
+		// WordPress Database handler.
+		global $wpdb;
+
+		$table = $wpdb->prefix . 'smaily_abandoned_carts';
+		$wpdb->update(
+			$table,
+			array(
+				'mail_sent'      => 1,
+				'mail_sent_time' => gmdate( 'Y-m-d\TH:i:s\Z' ),
+			),
+			array(
+				'customer_id' => $customer_id,
+			)
+		);
+	}
+
+	/**
+	 * Get abandoned carts from smaily_abandoned_carts table
+	 *
+	 * @return array
+	 */
+	public function get_abandoned_carts() {
+		// WordPress Database handler.
+		global $wpdb;
+		// Get all abandoned carts.
+		return $wpdb->get_results(
+			"
+			SELECT * FROM {$wpdb->prefix}smaily_abandoned_carts
+			WHERE cart_status='abandoned'
+			AND mail_sent IS NULL
+			",
+			'ARRAY_A'
+		);
 	}
 
 	/**
